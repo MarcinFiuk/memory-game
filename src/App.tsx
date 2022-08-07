35125e214245c5a3aa1b0ac8ecb1329fdb2b5def
Marcin_Fi <fiukmarcinn@gmail.com>
2022-08-07 11:23:39 +0100

    style: setup basic style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
     return (
-        <h1 className='text-3xl text-orange-100 font-bold underline'>
-            Hello world!
-        </h1>
+        <>
+            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+            <p>secons</p>
+            <a href='/'>kfsahfsakl</a>
+        </>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    content: ['./*/index.html', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                body: ['Atkinson Hyperlegible', 'Roboto'],
+            },
+            colors: {
+                'clr-primary-900': 'hsl(205,30%,27%)',
+                'clr-primary-500': 'hsl(37,98%,54%)',
+                'clr-primary-300': 'hsl(203,28%,79%)',
+                'clr-accent-900': 'hsl(206,45%,15%)',
+                'clr-accent-500': 'hsl(205,37%,55%)',
+                'clr-accent-300': 'hsl(203,25%,90%)',
+                'clr-neutral-100': 'hsl(0,0%,99%)',
+            },
+        },
     },
     plugins: [],
 };
